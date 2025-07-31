faecb5376375fdf2b0dbfc1eb1958c0ff4e95343
Nicolas Diaz <nicobueno2001@gmail.com>
2025-07-31 18:37:16 -0500

    Subir proyecto

--- react/src/axios.js
+++ react/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: `http://127.0.0.1:8000/api`,
+  baseURL: `https://decameron-taupe.vercel.app/api/api/`,
 });
 
 axiosClient.interceptors.response.use(
